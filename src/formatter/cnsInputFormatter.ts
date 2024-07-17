2efb49c19f011dd959862347785c4b70a53aa6cb
EdgarJMesquita <edgaroboe@outlook.com>
2024-07-17 02:42:13 -0300

    :bug: fix: typo cestInputFormatter->cnsInputFormatter

--- src/formatter/cnsInputFormatter.ts
+++ src/formatter/cnsInputFormatter.ts
@@ -1,6 +1,6 @@
 import { MaskType } from './constants/maskType';
 import { baseNumberMaskHandler } from './utils/baseNumberMaskHandler';
 
-export function cestInputFormatter(callback: (value: string) => void) {
+export function cnsInputFormatter(callback: (value: string) => void) {
   return (value: any) => callback(baseNumberMaskHandler(value, MaskType.CNS));
 }
